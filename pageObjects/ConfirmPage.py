2ac638a6d1b7ac05dd9b3b829dc94a11f78b0cd0
Andrew Lam <AndrewLam489@Gmail.com>
2025-04-18 00:14:47 -0400

    Update ConfirmPage

--- pageObjects/ConfirmPage.py
+++ pageObjects/ConfirmPage.py
@@ -1,11 +1,24 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 class ConfirmPage:
     def __init__(self, driver):
         self.driver = driver
 
     deliveryLocation = (By.ID,"country")    #self.driver.find_element(By.ID, "country").send_keys("Ind")  # Type 'Ind' in the country search field
+    dropdownIndia = (By.LINK_TEXT,"India")  #self.driver.find_element(By.LINK_TEXT, "India").click()  # Click on the 'India' link
+    purchaseBtn = (By.XPATH,"//div[@class='checkbox checkbox-primary']")    #self.driver.find_element(By.XPATH, "//div[@class='checkbox checkbox-primary']").click()
 
 
     def getDeliveryLocation(self):
         return self.driver.find_element(*ConfirmPage.deliveryLocation)
+    
+    def getDropdownIndia(self):
+        wait = WebDriverWait(self.driver, 10)
+        wait.until(EC.presence_of_element_located((By.LINK_TEXT, "India")))
+        return self.driver.find_element(*ConfirmPage.dropdownIndia)
+    
+    def getPurchaseButton(self):
+        return self.driver.find_element(*ConfirmPage.purchaseBtn)
+    
