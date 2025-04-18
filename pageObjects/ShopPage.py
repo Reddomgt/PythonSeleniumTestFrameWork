0e495a10a358cb7f760024157f72d1525684cc58
Andrew Lam <AndrewLam489@Gmail.com>
2025-04-18 02:02:17 -0400

    Updating ShopPage.py docstrings

--- pageObjects/ShopPage.py
+++ pageObjects/ShopPage.py
@@ -1,22 +1,58 @@
 from selenium.webdriver.common.by import By
 
 class ShopPage:
+    """
+    Page Object Model for the Shop Page in the application.
+    Provides methods to interact with elements on the Shop Page.
+    """
+
     def __init__(self, driver):
+        """
+        Initializes the ShopPage object.
+
+        Args:
+            driver: The WebDriver instance used to interact with the browser.
+        """
         self.driver = driver
 
-    cardTitle = (By.CSS_SELECTOR, ".card-title a")                      #driver.find_elements(By.CSS_SELECTOR, ".card-title a")
-    cardFooter = (By.CSS_SELECTOR, ".card-footer button")               #driver.find_elements(By.CSS_SELECTOR, ".card-footer button")
-    shopCheckout = (By.XPATH, "//a[@class='nav-link btn btn-primary']") #driver.find_element(By.XPATH, "//a[@class='nav-link btn btn-primary']")
-    cartCheckout = (By.XPATH, "//button[@class='btn btn-success']")   #driver.find_element(By.XPATH, "//button[@class='btn btn-success']")
+    # Locators for elements on the Shop Page
+    cardTitle = (By.CSS_SELECTOR, ".card-title a")  # Locator for the titles of the product cards
+    cardFooter = (By.CSS_SELECTOR, ".card-footer button")  # Locator for the 'Add to Cart' buttons in the product cards
+    shopCheckout = (By.XPATH, "//a[@class='nav-link btn btn-primary']")  # Locator for the shop checkout button in the navigation bar
+    cartCheckout = (By.XPATH, "//button[@class='btn btn-success']")  # Locator for the cart checkout button
 
     def getCardTitles(self):
+        """
+        Gets all product card title elements.
+
+        Returns:
+            list[WebElement]: A list of WebElements representing the product card titles.
+        """
         return self.driver.find_elements(*ShopPage.cardTitle)
     
     def getCardFooter(self):
+        """
+        Gets all 'Add to Cart' button elements in the product cards.
+
+        Returns:
+            list[WebElement]: A list of WebElements representing the 'Add to Cart' buttons.
+        """
         return self.driver.find_elements(*ShopPage.cardFooter)
     
     def shopCheckoutItems(self):
+        """
+        Gets the shop checkout button element in the navigation bar.
+
+        Returns:
+            WebElement: The shop checkout button element.
+        """
         return self.driver.find_element(*ShopPage.shopCheckout)
     
     def cartCheckoutItems(self):
+        """
+        Gets the cart checkout button element.
+
+        Returns:
+            WebElement: The cart checkout button element.
+        """
         return self.driver.find_element(*ShopPage.cartCheckout)
