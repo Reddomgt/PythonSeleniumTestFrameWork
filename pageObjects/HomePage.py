02744d070cdea0806c78bf7bc1ad3121ece1b2ff
Andrew Lam <AndrewLam489@Gmail.com>
2025-04-18 02:02:00 -0400

    Updating HomePage.py docstrings

--- pageObjects/HomePage.py
+++ pageObjects/HomePage.py
@@ -1,11 +1,28 @@
 from selenium.webdriver.common.by import By
 
 class HomePage:
-    def __init__(self,driver):
+    """
+    Page Object Model for the Home Page in the application.
+    Provides methods to interact with elements on the Home Page.
+    """
+
+    def __init__(self, driver):
+        """
+        Initializes the HomePage object.
+
+        Args:
+            driver: The WebDriver instance used to interact with the browser.
+        """
         self.driver = driver
 
-    shop = (By.CSS_SELECTOR, "a[href='/angularpractice/shop']")
+    # Locators for elements on the Home Page
+    shop = (By.CSS_SELECTOR, "a[href='/angularpractice/shop']")  # Locator for the 'Shop' link
 
     def shopItems(self):
-        #self.driver.find_element(By.CSS_SELECTOR, "a[href='/angularpractice/shop']").click()  # Click on the 'shop' link
+        """
+        Gets the 'Shop' link element.
+
+        Returns:
+            WebElement: The 'Shop' link element.
+        """
         return self.driver.find_element(*HomePage.shop)
